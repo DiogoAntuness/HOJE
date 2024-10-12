d69dd3a1c54c0296108914953e85ee5036e477c7
Diogo Antunes <diogoalves2684@gmail.com>
2024-10-12 14:22:14 -0300

    Complicado

---
+++ app1/__init__.py
@@ -0,0 +1,13 @@
+
+a = 5
+b = 7
+
+print('Resultado ', (a + b) / 2, 'ESSE AQUI: ', end ="")
+if a >= 8:
+    print("Legal")
+else:
+     print("Feio")
+
+
+for n in range(5, 10):
+     print("Valor: ", n)
